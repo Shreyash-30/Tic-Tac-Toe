9368d2e920160908dcdc38f032b5f1c8883cc8ee
Shreyash-30 <shreyash.30@gmail.com>
2025-07-16 06:47:01 +0530

    complete game

---
+++ Tic-Tac-Toe/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+import Board from './Components/Board'
+function App() {
+ 
+
+  return (
+    <div className='app'>
+
+     <Board/>
+     
+    </div>
+  )
+}
+
+export default App
